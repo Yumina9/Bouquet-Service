8ea17ac3de49939740fd5f527fab73dcfa952bb9
Yumina9 <saei5455@naver.com>
2020-07-16 22:33:52 +0900

    Change Sellder Header
    Signed-off-by: Yumina9 <saei5455@naver.com>

--- src/components/header/SellerHeader.tsx
+++ src/components/header/SellerHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SellerHeader = () => {
-  return <div>판매자 header 컴포넌트 입니다.</div>;
+  return <div>판매자 미나 컴포넌트 입니다.</div>;
 };
 
 export default SellerHeader;
