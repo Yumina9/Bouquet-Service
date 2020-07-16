c52a2cc11ff6c4eb5321f413ce83177d3c7fedf0
Yumina9 <saei5455@naver.com>
2020-07-16 22:44:57 +0900

    feat : button component apply typescript

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Block = styled.button`
+type BlockType = {
+  color: string;
+  bgColor: string;
+};
+
+const Block = styled.button<BlockType>`
   display: flex;
   color: ${(props) => props.color || 'black'};
   background-color: ${(props) => props.bgColor || 'white'};
   border-radius: 5px;
   padding: 5px;
 `;
-
-const Button = ({ color, bgColor, children }) => {
+type ButtonType = BlockType & {
+  children: React.ReactNode;
+};
+const Button = ({ color, bgColor, children }: ButtonType) => {
   return (
     <Block type="button" color={color} bgColor={bgColor}>
       {children}
