dab563dfffb055527fd6ab9c6e56ce6a98300ac8
Yumina9 <saei5455@naver.com>
2020-07-19 19:06:42 +0900

    SignUp 오타수정

--- src/router/Login/SignUp.tsx
+++ src/router/Login/SignUp.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import SignUpForm from '../../components/login/SignUpForm';
 
-const SellerHeader = () => {
+const SignUp = () => {
   return <SignUpForm />;
 };
 
-export default SellerHeader;
+export default SignUp;
