66560de396a1a86a7f2c355905f43e44df693a95
Yumina9 <saei5455@naver.com>
2020-07-19 19:05:32 +0900

    feat: change MiniHeader

--- src/components/header/MiniHeader.tsx
+++ src/components/header/MiniHeader.tsx
@@ -4,18 +4,18 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+import palette from '../lib/styles/palette';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+    },
+    header: {
+      backgroundColor: palette.color5,
     },
     menuButton: {
       marginRight: theme.spacing(2),
@@ -28,13 +28,9 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export const MiniHeader = () => {
   const classes = useStyles();
-  const [auth, setAuth] = React.useState(true);
+  const auth = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-  };
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -46,30 +42,10 @@ export const MiniHeader = () => {
 
   return (
     <div className={classes.root}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup>
-      <AppBar position="static">
+      <AppBar className={classes.header} position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Photos
+          <Typography variant="h4" className={classes.title}>
+            꽃다발 주문제작 서비스
           </Typography>
           {auth && (
             <div>
