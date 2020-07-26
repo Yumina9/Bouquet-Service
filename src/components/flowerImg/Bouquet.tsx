bda241e7a9978f008da0d7bdfac3f758757daa72
Yumina9 <saei5455@naver.com>
2020-07-26 23:51:56 +0900

    Django와 FlowerList 연결 & Bouquet Component Material-ui 적용

--- src/components/flowerImg/Bouquet.tsx
+++ src/components/flowerImg/Bouquet.tsx
@@ -2,23 +2,99 @@ import React from 'react';
 import styled from 'styled-components';
 import Typography from '../common/Typography';
 import palette from '../lib/styles/palette';
-import { BouquetType } from '../../DummyData';
+import { FlowerType } from './FlowerList';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-const Bouquet: React.FC<BouquetType> = ({ title, img, id }) => {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '100%',
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  })
+);
+
+const Bouquet: React.FC<FlowerType> = ({ name, img, id, mean, color }) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <Card>
-      <Thumbnail src={img} />
-      <Typography type="H5" color={palette.color4} fontWeight="light">
-        {title}
-      </Typography>
-    </Card>
+    <>
+      <Box>
+        <Card className={classes.root}>
+          <CardHeader
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={name}
+            subheader="언제 올렸을까요?"
+          />
+          <CardMedia className={classes.media} image={img} title={name} />
+          <CardContent>
+            <Typography type="H7" color={palette.color4} fontWeight="light">
+              {color} {name}의 꽃말 : {mean}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            ></IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit></Collapse>
+        </Card>
+      </Box>
+    </>
   );
 };
 
 export default Bouquet;
 
-const Card = styled.span`
-  display: grid;
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin: 10px;
 `;
-
-const Thumbnail = styled.img``;
--- src/components/flowerImg/FlowerList.tsx
+++ src/components/flowerImg/FlowerList.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import { BouquetDummy } from '../../DummyData';
-import { BouquetType } from '../../DummyData';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import Bouquet from './Bouquet';
+import axios from 'axios';
+
+export type FlowerType = {
+  id: number;
+  name: string;
+  img: string;
+  mean: string;
+  color: string;
+};
 
 const FlowerList = () => {
+  const [flowers, setFlowers] = useState<FlowerType[]>([]);
+  useEffect(() => {
+    axios.get('/flowers').then(({ data }) => setFlowers(data));
+  }, []);
   return (
-    <>
-      {BouquetDummy.map((bouquet: BouquetType) => (
-        <Bouquet id={bouquet.id} img={bouquet.img} title={bouquet.title} />
-      ))}
-    </>
+    <List>
+      {flowers.map(({ id, name, img, mean, color }) => {
+        return (
+          <Bouquet id={id} name={name} img={img} mean={mean} color={color} />
+        );
+      })}
+    </List>
   );
 };
 
 export default FlowerList;
+
+const List = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+`;
