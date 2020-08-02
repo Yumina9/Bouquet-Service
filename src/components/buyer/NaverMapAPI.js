7f22daa11272e7eb6c1e4a583b28630842f8d792
miok-jung <j-narin@naver.com>
2020-08-02 18:48:32 +0900

    지도 일시중지 로그인수정

--- src/components/buyer/NaverMapAPI.js
+++ src/components/buyer/NaverMapAPI.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps';
 
 const NaverMapAPI = () => {
   const navermaps = window.naver.maps;
+  console.log(navermaps);
+  const userLatLon = navermaps.LatLng();
+  console.log(userLatLon);
+  // const userLocation = window.naver.maps.LatLng(
+  //   position.coords.latitude,
+  //   position.coords.longitude
+  // );
+  // console.log(userLocation);
+
+  // 위치 추적
+  // navigator.geolocation.getCurrentPosition(
+  //   function (position) {
+  //     const latitude = position.coords.latitude;
+  //     const longitude = position.coords.longitude;
+  //     console.log('위도 : ' + latitude);
+  //     console.log('경도 : ' + longitude);
+  //     console.log('First Location', position.coords);
+  //   },
+  //   function (error) {
+  //     console.log(error);
+  //   }
+  // );
+
+  // 사용자 위치표시 좌표
+  const [userLocation, setUserLocation] = useState({
+    // LatLng: (37.3595704, 127.105399),
+    LatLng: 0,
+  });
   return (
     <NaverMap
-      mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+      id="maps-examples-marker"
       style={{
-        width: '100%', // 네이버지도 가로 길이
-        height: '500px', // 네이버지도 세로 길이
+        width: '100%',
+        height: '500px',
       }}
-      defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
-      defaultZoom={13} // 지도 초기 확대 배율
+      // defaultCenter={userLocation.LatLng}
+      defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
+      defaultZoom={15}
     >
       <Marker
-        key={1}
-        position={new navermaps.LatLng(37.551229, 126.988205)}
-        animation={2}
+        position={new navermaps.LatLng(37.3595704, 127.105399)}
+        animation={navermaps.Animation.BOUNCE}
         onClick={() => {
-          alert('여기는 N서울타워입니다.');
+          alert('여기는 네이버 입니다.');
         }}
       />
     </NaverMap>
@@ -58,14 +86,14 @@ export default App;
 //       console.log(error);
 //     }
 //   );
-//   // 사용자 위치표시 좌표
+// 사용자 위치표시 좌표
 //   const [userLocation, setUserLocation] = useState({
 //     latitude: 37.4652876, // number 초기값은 0
 //     longitude: 126.900341,
 //   });
-//   // 꽃다발 주변 위치 리스트
+// 꽃다발 주변 위치 리스트
 //   const [markerPositions, setMarkerPositions] = useState([
-//     // 배열에 접근하는 방법 Map()
+// 배열에 접근하는 방법 Map()
 //     {
 //       key: 0,
 //       latitude: 37.3633324,
@@ -83,9 +111,9 @@ export default App;
 //     },
 //   ]);
 
-//   // const markerPositions = array.map((item) => {
-//   //   return {item.latitude} && {item.longitude}
-//   // });
+// const markerPositions = array.map((item) => {
+//   return {item.latitude} && {item.longitude}
+// });
 //   return (
 //     <NaverMap
 //       id="naver-map"
@@ -97,10 +125,10 @@ export default App;
 //       defaultCenter={userLocation}
 //     >
 //       <Marker
-//         // key={markerPositions.key} // 순서? 확인해야할 것
+// key={markerPositions.key} // 순서? 확인해야할 것
 //         key={0}
-//         // position={markerPositions.latitude}
-//         // position={markerPositions.longitude}
+// position={markerPositions.latitude}
+// position={markerPositions.longitude}
 //         position={(37.3633324, 129.1054988)}
 //         animation={1} // 0~2까지 애니메이션이 존재
 //       />
