721c6631f9cfed8d4e23a4edf040930854a07272
Yumina9 <saei5455@naver.com>
2020-08-04 16:49:32 +0900

    feat: change 선택된 카드 정보 출력

--- src/router/Seller/Confirmation.tsx
+++ src/router/Seller/Confirmation.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import { MiniHeader } from '../../components/header/MiniHeader';
 import Typography from '../../components/common/Typography';
 import palette from '../../components/lib/styles/palette';
 import Button from '../../components/common/Button';
-import { Link } from '@material-ui/core';
+import { useParams } from 'react-router-dom';
+import { BouquetType } from '../../components/flowerImg/FlowerList';
 
-const Confirmation = () => {
+const Confirmation: React.FC<BouquetType> = () => {
+  const { id } = useParams();
+  const [info, setInfo] = useState<BouquetType>();
+  useEffect(() => {
+    axios.get(`/bouquets/${id}`).then(({ data }) => setInfo(data));
+  }, []);
+  console.log(info);
   return (
     <div>
       <MiniHeader />
@@ -19,25 +27,26 @@ const Confirmation = () => {
             꽃 종류
           </Typography>
           <Typography type="H7" color={palette.black} fontWeight="light">
-            빨간색장미
+            {`${info?.flower_color} ${info?.flower_name}`}
           </Typography>
           <Typography type="H5" color={palette.color4} fontWeight="bold">
             꽃 수량
           </Typography>
           <Typography type="H7" color={palette.black} fontWeight="light">
-            5송이
+            {`${info?.flower_count}송이`}
           </Typography>
           <Typography type="H5" color={palette.color4} fontWeight="bold">
             꽃다발 종류
           </Typography>
           <Typography type="H7" color={palette.black} fontWeight="light">
-            하트 꽃다발
+            {`${info?.name}`}
           </Typography>
           <Typography type="H5" color={palette.color4} fontWeight="bold">
             리본, 포장지 색상
           </Typography>
           <Typography type="H7" color={palette.black} fontWeight="light">
-            리본 : 검정색 | 포장지 : 하얀색
+            리본 : {`${info?.ribbon_color}`} | 포장지 :
+            {`${info?.wrappingpaper_color}`}
           </Typography>
         </Order>
         <Button color={palette.black} bgColor={palette.color3}>
