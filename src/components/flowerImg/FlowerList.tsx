286215a4f72e98d969aaeec73ed0aed02cccba8a
Yumina9 <saei5455@naver.com>
2020-08-05 09:50:23 +0900

    feat: add BouquetType

--- src/components/flowerImg/FlowerList.tsx
+++ src/components/flowerImg/FlowerList.tsx
@@ -10,12 +10,20 @@ export type FlowerType = {
   description: string;
   color: string;
 };
+export type BouquetType = FlowerType & {
+  flower_name: string;
+  flower_color: string;
+  flower_count: number;
+  ribbon_color: string;
+  wrappingpaper_color: string;
+};
 
 const FlowerList = () => {
   const [flowers, setFlowers] = useState<FlowerType[]>([]);
   useEffect(() => {
     axios.get('/flowers').then(({ data }) => setFlowers(data));
   }, []);
+  console.log(flowers);
   return (
     <List>
       {flowers.map(({ id, name, img, description, color }) => {
