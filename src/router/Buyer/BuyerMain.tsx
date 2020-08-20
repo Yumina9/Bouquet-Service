3cf53259ad065a6b509303d38d25f60aeb8483a0
Yumina9 <saei5455@naver.com>
2020-08-20 22:33:15 +0900

    feat: add MainInfo

--- src/router/Buyer/BuyerMain.tsx
+++ src/router/Buyer/BuyerMain.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {MiniHeader} from '../../components/header/MiniHeader';
-// import Header from '../../components/header/Header';
+import { MiniHeader } from '../../components/header/MiniHeader';
+import { MainInfo } from '../../components/header/MainInfo';
 import BestItemList from '../../components/buyer/BestItemList';
 import CustomSelect from '../../components/buyer/CustomSelect';
 
 const BuyerMain = () => {
-    return (
-        <div>
-            <MiniHeader />
-            <BestItemList />
-            <CustomSelect />
-        </div>
-    );
+  return (
+    <div>
+      <MiniHeader />
+      <MainInfo />
+      <BestItemList />
+      <CustomSelect />
+    </div>
+  );
 };
 
 export default BuyerMain;
