4774344f379a8fca76ed974d429297b7d3d0a237
Yumina9 <saei5455@naver.com>
2020-09-02 22:39:07 +0900

    feat: add flower image MakingFlower component

--- src/components/Making/MakingFlowerStepper.tsx
+++ src/components/Making/MakingFlowerStepper.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import palette from '../lib/styles/palette';
 import Button from '../common/Button';
+import { BouquetType } from '../flowerImg/FlowerList';
+import Axios from 'axios';
+import Typography from '../common/Typography';
 
 export default function MakingFlowerStepper() {
+  const { id } = useParams();
+  const [flowers, setFlowers] = useState<BouquetType[]>([]);
+  useEffect(() => {
+    Axios.get('/flowers/').then(({ data }) => setFlowers(data));
+  }, []);
   return (
     <>
+      <span>
+        <img src={`${flowers?.img}`} style={{ width: '400px' }} />
+        <Typography type="H5" color={palette.color4} fontWeight="middle">
+          {`${flowers?.name}`}
+        </Typography>
+      </span>
       <div>
         <p>꽃을 선택하셧습니다.</p>
       </div>
