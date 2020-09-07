90f806987d0a784b552838f842d3315faa6e293b
miok-jung <j-narin@naver.com>
2020-09-08 00:58:31 +0900

    buyermypage 디자인 작업

--- src/router/Seller/SellerMyPage.tsx
+++ src/router/Seller/SellerMyPage.tsx
@@ -30,13 +30,16 @@ const Info = styled.div`
   display: flex;
   & > :first-child {
     flex: 4;
+    border: 1px solid blue;
   }
   & > :last-child {
     flex: 6;
+    border: 1px solid lime;
   }
   padding: 50px 100px;
   div {
     margin: 25px;
     justify-content: left;
+    background: #f1f2f3;
   }
 `;
