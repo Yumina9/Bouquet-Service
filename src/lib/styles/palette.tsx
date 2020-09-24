c110890b4dfca537acedb3f1c470b572861a7225
Yumina9 <saei5455@naver.com>
2020-09-24 21:18:08 +0900

    feat: add color to the palette

--- src/lib/styles/palette.tsx
+++ src/lib/styles/palette.tsx
@@ -4,8 +4,13 @@ const palette = {
   color3: '#FFA7B3',
   color4: '#402A2D',
   color5: '#E596A1',
+  color6: '#F2C1BD',
+  color7: '#F2B6B6',
+  background: '#F2D3D0',
   white: '#FFF',
   black: '#111',
+  lightgray: '#A6A6A6',
+  gray: '#737373',
 } as const;
 
 export default palette;
