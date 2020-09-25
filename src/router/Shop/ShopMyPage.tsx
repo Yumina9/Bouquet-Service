798b15749bc5ec5d06c209ff49d3822693093069
Yumina9 <saei5455@naver.com>
2020-09-25 19:09:35 +0900

    Refactoring : structure change to the ShopMyPage Page

--- src/router/Shop/ShopMyPage.tsx
+++ src/router/Shop/ShopMyPage.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
@@ -14,6 +16,7 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
+import Button from '../../components/common/Button';
 
 const drawerWidth = 240;
 
@@ -50,6 +53,18 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function ClippedDrawer() {
   const classes = useStyles();
 
+  const Panel = ({ e }: number) => {
+    switch (e) {
+      case 1:
+        return <>내정보</>;
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  };
+
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -72,72 +87,41 @@ export default function ClippedDrawer() {
       >
         <Toolbar />
         <div className={classes.drawerContainer}>
-          <List>
-            {['내 정보'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['보유 꽃다발', '보유 꽃'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['현재 주문 목록', '주문 완료 목록'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
+          <>
+            <Divider />
+            <ButtonItem
+              value="1"
+              onClick={() => {
+                Panel(1);
+              }}
+            >
+              내 정보
+            </ButtonItem>
+            <Divider />
+            <ButtonItem value={2}>보유 꽃다발</ButtonItem>
+            <ButtonItem value={3}>보유 꽃</ButtonItem>
+            <Divider />
+            <ButtonItem value={4}>현재 주문 목록</ButtonItem>
+            <ButtonItem value={5}>주문 완료 목록</ButtonItem>
+            <Divider />
+          </>
         </div>
       </Drawer>
       <main className={classes.content}>
         <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
       </main>
     </div>
   );
 }
+
+const ButtonItem = styled.button`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-size: 1.3rem;
+  color: black;
+  background-color: white;
+  padding: 10px;
+  border: none;
+  outline: none !important;
+`;
