f6d094536fe28d2ecff6e53d7b0fb582d3b34368
Yumina9 <saei5455@naver.com>
2020-09-30 20:43:11 +0900

    feat: add FlowerList to the FlowerListPanel

--- src/components/Shop/Panel/FlowerListPanel.tsx
+++ src/components/Shop/Panel/FlowerListPanel.tsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import Axios from 'axios';
+import Flower, { FlowerType } from '../../flowerImg/Flower';
 
 export const FlowerListPanel = () => {
-  return <>꽃 리스트를 보여줍니다!</>;
+  const { id } = useParams();
+  const [flowers, setFlowers] = useState<FlowerType[]>([]);
+  useEffect(() => {
+    Axios.get(`/shop/${id}/flowers`).then(({ data }) => setFlowers(data));
+  }, []);
+  console.log(flowers);
+
+  return (
+    <>
+      <FlowerList>
+        {flowers.map(({ id, name, img, description, color }) => {
+          return (
+            <Flower
+              id={id}
+              name={name}
+              img={img}
+              description={description}
+              color={color}
+            />
+          );
+        })}
+      </FlowerList>
+    </>
+  );
 };
+
+const FlowerList = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+`;
