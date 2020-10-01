83fc089b75d9ec823ce4ccba32f4a88cd33510e9
Yumina9 <saei5455@naver.com>
2020-10-01 20:56:17 +0900

    feat: add BouquetList to the BouquetPanee

--- src/components/Shop/Panel/BouquetListPanel.tsx
+++ src/components/Shop/Panel/BouquetListPanel.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import Axios from 'axios';
+import Bouquet, { BouquetType } from '../../flowerImg/Bouquet';
 
 export const BouquetListPanel = () => {
-  return <>꽃다발 리스트를 보여줍니다!</>;
+  const { id } = useParams();
+  const [bouquets, setBouquets] = useState<BouquetType[]>([]);
+  useEffect(() => {
+    Axios.get(`/shop/${id}/bouquets`).then(({ data }) => setBouquets(data));
+  }, []);
+
+  return (
+    <>
+      <BouquetList>
+        {bouquets.map(({ id, name, img, description }) => {
+          return (
+            <Bouquet id={id} name={name} img={img} description={description} />
+          );
+        })}
+      </BouquetList>
+    </>
+  );
 };
+
+const BouquetList = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+`;
