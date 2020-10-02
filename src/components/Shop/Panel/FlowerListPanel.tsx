9ba0366ba805a906a3453df32e3d46c2015f8356
Yumina9 <saei5455@naver.com>
2020-10-02 22:46:45 +0900

    feat: add panel title to the FlowerListPanel

--- src/components/Shop/Panel/FlowerListPanel.tsx
+++ src/components/Shop/Panel/FlowerListPanel.tsx
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import Axios from 'axios';
 import Flower, { FlowerType } from '../../flowerImg/Flower';
+import Typography from '../../common/Typography';
+import palette from '../../../lib/styles/palette';
+import Button from '../../common/Button';
 
 export const FlowerListPanel = () => {
   const { id } = useParams();
@@ -14,6 +17,12 @@ export const FlowerListPanel = () => {
 
   return (
     <>
+      <Typography type="H4" color={palette.color3} fontWeight="bold">
+        현재 보유 중인 꽃 리스트
+      </Typography>
+      <Button color={palette.white} bgColor={palette.color3}>
+        추가하기
+      </Button>
       <FlowerList>
         {flowers.map(({ id, name, img, description, color }) => {
           return (
@@ -33,6 +42,5 @@ export const FlowerListPanel = () => {
 
 const FlowerList = styled.div`
   display: flex;
-  flex-wrap: nowrap;
-  justify-content: center;
+  flex-wrap: wrap;
 `;
