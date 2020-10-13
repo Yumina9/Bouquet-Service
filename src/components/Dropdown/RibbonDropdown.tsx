d4abbd9dcea2bd1ad4b91e27da40b22c62685742
Yumina9 <saei5455@naver.com>
2020-10-13 16:51:53 +0900

    Separating : RibbonDropdown files with custom hooks

--- src/components/Dropdown/RibbonDropdown.tsx
+++ src/components/Dropdown/RibbonDropdown.tsx
@@ -1,41 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import palette from '../../lib/styles/palette';
-import axios from 'axios';
-import { FlowerType } from '../flowerImg/Flower';
-import Button from '../common/Button';
-import { RibbonType } from '../flowerImg/Ribbon';
+import React from 'react';
 import { Dropdown } from './Dropdown';
 import { Select } from '@material-ui/core';
-import { useParams } from 'react-router-dom';
+import useRibbonDropdown from './hooks/useRibbonDropdown';
 
-export const RibbonDropdown: React.FC<{ onReserveChange: any }> = ({
-  onReserveChange,
-}) => {
-  const [ribbons, setRibbons] = useState<RibbonType[]>([]);
-  const [selected, setSelected] = useState<string | undefined>('');
-  const { shop_id: shopId } = useParams<{ shop_id: string }>();
-  useEffect(() => {
-    axios.get(`/shop/${shopId}/ribbons/`).then(({ data }) => setRibbons(data));
-  }, []);
-
-  console.log(ribbons);
-
-  const handleChange = (
-    event: React.ChangeEvent<{ name?: string; value: unknown }>,
-  ) => {
-    const ribbonIndex = ribbons
-      .map(function (ribbon) {
-        return ribbon.id;
-      })
-      .indexOf(Number(event.target.value));
-    const selectedRibbon = ribbons[ribbonIndex];
-
-    setSelected(event.target.value as string);
-
-    onReserveChange({ key: 'ribbon', data: selectedRibbon });
-  };
-
+export const RibbonDropdown: React.FC<{
+  onReserveChange: any;
+}> = ({ onReserveChange }) => {
+  const { ribbons, selected, handleChange } = useRibbonDropdown(
+    onReserveChange,
+  );
   return (
     <Dropdown description={'리본 선택'}>
       <Select
