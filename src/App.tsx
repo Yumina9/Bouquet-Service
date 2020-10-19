153541d2dc221735035caa07e25bd075f095999a
Yumina9 <saei5455@naver.com>
2020-10-19 12:07:59 +0900

    feat: user data 전역으로 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.less';
 import Router from './components/Router';
 import { getUserToken } from './components/login/authUtils';
 import axios from 'axios';
+import axiosInstance from './components/login/axios';
+import { useDispatch } from 'react-redux';
+import { login } from './modules/user';
 
 axios.defaults.headers.common['Authorization'] = `Token ${getUserToken()}`;
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axiosInstance.get(`user/me`).then(({ data }) => dispatch(login(data)));
+  }, []);
+
   return <Router />;
 }
 
---
+++ src/modules/user.js
@@ -0,0 +1,21 @@
+const USER_LOGIN = 'USER_LOGIN';
+
+export const login = (user) => ({
+  type: USER_LOGIN,
+  user,
+});
+
+const initialState = {
+  user: null,
+};
+
+function user(state = initialState, action) {
+  switch (action.type) {
+    case USER_LOGIN:
+      return { ...state, user: action.user };
+    default:
+      return state;
+  }
+}
+
+export default user;
