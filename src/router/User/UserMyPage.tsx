43bfa489d12f85eb24aa1c0e1b690194dcd6fc9c
Yumina9 <saei5455@naver.com>
2020-10-22 23:58:24 +0900

    UserMyPage 안쓰는 코드 정리

--- src/router/User/UserMyPage.tsx
+++ src/router/User/UserMyPage.tsx
@@ -4,16 +4,10 @@ import Header from '../../components/header/Header';
 import './buyerCss.css';
 import axiosInstance from '../../components/login/axios';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Footer from '../Footer';
-import { DataGrid } from '@material-ui/data-grid';
-import {
-  BouquetOrderType,
-  CurrentOrderPanel,
-} from '../../components/Shop/Panel/CurrentOrderPanel';
-import palette from '../../lib/styles/palette';
+import { BouquetOrderType } from '../../components/Shop/Panel/CurrentOrderPanel';
 import Divider from '@material-ui/core/Divider';
 
 type userInfoType = {
@@ -50,44 +44,9 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const UserMyPage = () => {
-  // const columns = [
-  //   { field: 'id', headerName: '숫자', width: 70 },
-  //   { field: 'shopName', headerName: '매장명', width: 130 },
-  //   { field: 'Flowlist', headerName: '플로리스트', width: 130 },
-  //   {
-  //     field: 'price',
-  //     headerName: '주문금액',
-  //     type: 'number',
-  //     width: 130,
-  //   },
-  //   {
-  //     field: 'content',
-  //     headerName: '주문내역',
-  //     description: 'This column has a value getter and is not sortable.',
-  //     sortable: false,
-  //     width: 160,
-  //     valueGetter: (params) =>
-  //       `${params.getValue('firstName') || ''} ${
-  //         params.getValue('lastName') || ''
-  //       }`,
-  //   },
-  // ];
-
-  // const rows = [
-  //   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  //   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  //   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  //   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  //   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  //   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  //   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  //   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  //   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  // ];
-
   const [userInfo, setUserInfo] = useState<userInfoType>();
-  const [order, setOrder] = useState<BouquetOrderType | undefined>();
-  const [userOrder, setUserOrder] = useState<BouquetOrderType | undefined>();
+  const [order] = useState<BouquetOrderType | undefined>();
+  const [userOrder] = useState<BouquetOrderType | undefined>();
 
   useEffect(() => {
     axiosInstance.get(`/user/me`).then(({ data }) => {
