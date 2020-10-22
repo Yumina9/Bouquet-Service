527f20fa767d3aeb90e2dcb605f896bbcebff606
Yumina9 <saei5455@naver.com>
2020-10-22 17:59:04 +0900

    Change: orderConfirmData

--- src/components/OrderConfirm/OrderConfirmData.tsx
+++ src/components/OrderConfirm/OrderConfirmData.tsx
@@ -11,6 +11,7 @@ import { RootState } from '../../modules';
 export const OrderConfirmData = () => {
   const { order } = useOrderConfirmForm();
   const OrderData = useSelector((state: RootState) => state?.order);
+  console.log('orderdt', OrderData);
 
   return (
     <>
@@ -29,7 +30,7 @@ export const OrderConfirmData = () => {
             꽃 종류
           </Typography>
           <p />
-          {OrderData?.flower}
+          {OrderData?.flower.name}
           <p />
           <Typography type="H5" color={palette.color4} fontWeight="bold">
             꽃 수량
@@ -44,7 +45,8 @@ export const OrderConfirmData = () => {
           </Typography>
           <p />
           <Typography type="H7" color={palette.black} fontWeight="light">
-            리본 :{OrderData?.ribbon} | 포장지 :{OrderData?.wrappingPaper}
+            리본 :{OrderData?.ribbon.name} | 포장지 :
+            {OrderData?.wrappingPaper.name}
           </Typography>
           <p />
           <Typography type="H5" color={palette.color4} fontWeight="bold">
